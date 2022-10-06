86b6ac1fec7aaa11a16e3e9e49b805cfa1cf7b62
IanMwaniki <ian.mwaniki@student.moringaschool.com>
2022-10-07 01:49:25 +0300

    Cards and slides created

--- delicious/src/components/Popular.jsx
+++ delicious/src/components/Popular.jsx
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import {Splide, SplideSlide} from "@splidejs/react-splide";
+import '@splidejs/react-splide/css';
 
 function Popular(){
     const [popular, setPopular] = useState([]);
@@ -12,6 +15,7 @@ const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${pro
 const data = await api.json();
 // console.log(data);
 setPopular(data.recipes);
+console.log(data.recipes);
     }
 
 //going through the fetched data recipes to dispaly them through maping
@@ -19,13 +23,44 @@ setPopular(data.recipes);
         <div>
             {popular.map(recipe =>{
        return(
-       <div key={recipe.id}>
-        <p>{recipe.title}</p>
-       </div>
+    //    <div key={recipe.id}>
+    //     <p>{recipe.title}</p>
+    //    </div>
+    <Wrapper>
+        <h3>Popular Picks</h3>
+        
+        <Splide>
+        {popular.map((recipe) => {
+            return (
+                <SplideSlide>
+           <Card>
+            <p>{recipe.title}</p>
+            <img src={recipe.image} alt={recipe.title}/>
+           </Card>
+           </SplideSlide>
+            );
+        })}
+        </Splide>
+
+    </Wrapper>
        );
             })}
         </div>
     )
 }
+
+const Wrapper = styled.div`
+margin: 4rem Orem;
+`
+
+const Card = styled.div`
+min-height: 25rem;
+border-radius: 2rem;
+overflow: hidden;
+
+ img{
+    border-radius: 2rem;
+}
+`
 
 export default Popular;
