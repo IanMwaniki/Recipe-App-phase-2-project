8e414eb705ee84aea2fbbaf4874bff3cc4f83f26
IanMwaniki <ian.mwaniki@student.moringaschool.com>
2022-10-06 20:45:38 +0300

    Data fetched from API

--- delicious/src/components/Popular.jsx
+++ delicious/src/components/Popular.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Popular(){
+    const [popular, setPopular] = useState([]);
+
+useEffect(() => {
+    getPopular();
+},[]);
+
+    const getPopular = async () => {
+const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`);
+const data = await api.json();
+// console.log(data);
+setPopular(data.recipes);
+    }
+
+//going through the fetched data recipes to dispaly them through maping
     return (
         <div>
-            Popular
+            {popular.map(recipe =>{
+       return(
+       <div key={recipe.id}>
+        <p>{recipe.title}</p>
+       </div>
+       );
+            })}
         </div>
     )
 }
